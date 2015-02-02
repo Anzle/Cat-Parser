5d0e8bc77b832a3cdd1b3c885d1efde5c7a0b2b1
Rob <rlw115@scarletmail.rutgers.edu>
2015-02-01 19:57:13 -0500

    Sooooooo.... DUCT TAPE
    It's shotty and it really needs more work.
    
    Main issue right now is making sure that the result string is properly
    managed memory wise. It will need to be freed, btu that is causing
    issues. Currently, the program will loop forever.
    while(pos < length) will have a result of 8 < 9 so that is something
    else to fix. will it parse one string? yes. Can I fix this memory
    management? With time.
    
    I find professors function headers, comments and lack of direction
    irksome. Should discuss.

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -1,24 +1,27 @@
 /*
- * tokenizer.c
- */
+* tokenizer.c
+*/
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
+/*Globals*/
 int pos = 0; //index location of where the tokenizer is in the string
 char* input; // This will reference the address of argv[1]
 
 /* Library of String Constants used in Tokenizer*/
- char* WORD = "word";
- char* INT = "integer";
- char* HEX = "hex";
- char* OCT = "oct";
- char* FLOAT = "float";
+char* WORD = "word";
+char* INT = "integer";
+char* HEX = "hex";
+char* OCT = "oct";
+char* FLOAT = "float";
 
 //ect
 
 /*
- * Tokenizer type.  You need to fill in the type as part of your implementation.
- */
+* Tokenizer type.  You need to fill in the type as part of your implementation.
+*/
 struct TokenizerT_ {
 	char *token; //The current token/piece of the string being analyzed
 	char *type; //This is the type of token, points to an entry in the type table
@@ -27,172 +30,217 @@ struct TokenizerT_ {
 typedef struct TokenizerT_ TokenizerT;
 
 /*
- * TKCreate creates a new TokenizerT object for a given token stream
- * (given as a string).
- * 
- * TKCreate should copy the arguments so that it is not dependent on
- * them staying immutable after returning.  (In the future, this may change
- * to increase efficiency.)
- *
- * If the function succeeds, it returns a non-NULL TokenizerT.
- * Else it returns NULL.
- *
- * You need to fill in this function as part of your implementation.
- */
+* TKCreate creates a new TokenizerT object for a given token stream
+* (given as a string).
+*
+* TKCreate should copy the arguments so that it is not dependent on
+* them staying immutable after returning.  (In the future, this may change
+* to increase efficiency.)
+*
+* If the function succeeds, it returns a non-NULL TokenizerT.
+* Else it returns NULL.
+*
+* You need to fill in this function as part of your implementation.
+*/
 
- /* Only call this method once, to set up the initial tokenizer. Then the 
-  * TKGetNextToken uses the global string index (strloc declared above)
-  * to keep track of where in str we are and what token to be stored in 
-  * the tokenizer. 
-  */
-TokenizerT *TKCreate( char * ts ) {
-	if(ts == 0){
+/* Only call this method once, to set up the initial tokenizer. Then the
+* TKGetNextToken uses the global string index (strloc declared above)
+* to keep track of where in str we are and what token to be stored in
+* the tokenizer.
+*/
+TokenizerT *TKCreate(char * ts) {
+	if (ts == 0){
 		return NULL;
 	}
 	else {
-		TokenizerT *tokenizer = (TokenizerT *) calloc(1,sizeof(TokenizerT));
-		tokenizer -> token = ts; //technically don't need this since gettoken method returns a char*
-		tokenizer -> type = NULL;
+		TokenizerT *tokenizer = (TokenizerT *)calloc(1, sizeof(TokenizerT));
+		tokenizer->token = ts; //technically don't need this since gettoken method returns a char*
+		tokenizer->type = NULL;
 		return tokenizer;
 	}
 }
 
 /*
- * TKDestroy destroys a TokenizerT object.  It should free all dynamically
- * allocated memory that is part of the object being destroyed.
- *
- * You need to fill in this function as part of your implementation.
- */
+* TKDestroy destroys a TokenizerT object.  It should free all dynamically
+* allocated memory that is part of the object being destroyed.
+*
+* You need to fill in this function as part of your implementation.
+*/
 
-void TKDestroy( TokenizerT * tk ) {
-	free(tk -> token); 
+void TKDestroy(TokenizerT * tk) {
+	free(tk->token);
 	//free(tk -> type); //no need to free
 	free(tk);
 }
 
-/*
- * TKGetNextToken returns the next token from the token stream as a
- * character string.  Space for the returned token should be dynamically
- * allocated.  The caller is responsible for freeing the space once it is
- * no longer needed.
- *
- * If the function succeeds, it returns a C string (delimited by '\0')
- * containing the token.  Else it returns 0.
- *
- * You need to fill in this function as part of your implementation.
- */
 
-char *TKGetNextToken( TokenizerT * tk ) {
-
-  return NULL;
-} 
- 
- 
- /* This function is a wrapper function to expand the header options provided
-  * by the code given to us by the professor. build token creates a token and then
-  * gives the token a type for use later on  
-  */
- TokenizerT* buildToken(char* ts, char* type){
-	TokenizerT* t = TKCreate( char * ts );
-	t -> type = type;
+/* This function is a wrapper function to expand the header options provided
+* by the code given to us by the professor. build token creates a token and then
+* gives the token a type for use later on
+*/
+TokenizerT* buildToken(char* ts, char* type){
+	TokenizerT* t = TKCreate(ts);
+	t->type = type;
 	return t;
- };
- 
- /* The function creates a token, from the first word of the input string
-  Input: String to be parsed
-  Output: TokenizerT pointer to the created token
- */
- TokenizerT* stringParse(){
+};
+
+/* The function creates a token, from the first word of the input string
+Input: String to be parsed
+Output: TokenizerT pointer to the created token
+*/
+TokenizerT* stringParse(){
 	TokenizerT* tok = NULL;
+	char* token;
 	int length = strlen(input) - pos; //length of input string
+	int len = 0;
 	//do{
-		//Input is a word
-		if(isalphainput[pos]){
-			while(isalnum(input[++pos]) 
+	//Input is a word
+	if (isalpha(input[pos])){
+		len++;
+		while (isalnum(input[++pos])){ len++; }
 			//Create substring
-			char* token = malloc(sizeof(char)*pos+1);
-			 tok = buildToken(strncpy(input, token, pos), WORD);
-		}		
-			//input is a digit, what kind?
-		else if (isdigit(input[pos])){
-				//HEX
-			if(input[pos] == 0x30){//Check for 0 
-				pos++; //and 0x/0X, make sure not at the end of the string
-				if((pos) != length && (input[pos] == 0x58 || input[pos] == 0x78))
-				{
-					while(isxdigit(input[++pos]) 
-					char* token = malloc(sizeof(char)*pos+1);
-					tok = buildToken(strncpy(input, token, pos), HEX);	
-					
-				}
-				//OCT
-				else
-				{
-					while(isdigit(input[++pos]) 
-					char* token = malloc(sizeof(char)*pos+1);
-					tok = buildToken(strncpy(input, token, pos), OCT);
+		token = malloc(sizeof(char)*len + 1);
+		strncpy(token, input, len);
+		token[len] = '\0';
+		tok = buildToken(token, WORD);
+	}
+	//input is a digit, what kind?
+	else if (isdigit(input[pos])){
+		len++;
+		//HEX
+		if (input[pos] == 0x30){//Check for 0 
+			pos++; //and 0x/0X, make sure not at the end of the string
+			if ((pos) != length && (input[pos] == 0x58 || input[pos] == 0x78))
+			{
+				while (isxdigit(input[++pos])){ len++; }
+				token = malloc(sizeof(char)*len + 1);
+				strncpy(token, input,len);
+				token[len] = '\0';
+				tok = buildToken(token, HEX);
+
+			}
+			//OCT
+			else
+			{
+				while (isdigit(input[++pos])){ len++; }
+				token = malloc(sizeof(char)*len + 1);
+				strncpy(token, input, len);
+				token[len] = '\0';
+				tok = buildToken(token, OCT);
+			}
+		}
+		else{ //The digit is an integer or a float
+			char t = 'd';
+			//INTEGER
+			while (!isspace(input[++pos])){
+				if (isdigit(input[pos])){}
+				//Float
+				else if (tolower(input[pos]) == 0x65 || input[pos] == 0x2e){
+					t = 'f'; //is float
 				}
 			}
-			else{ //The digit is an integer or a float
-				char t = 'd';
-				//INTEGER
-				while(!isspace(input[++pos])){
-					if(isdigit(input[pos]){}
-					//Float
-					else if(toloswer(input[pos]) ==  0x65 || input[pos] == 0x2e){
-						t = 'f'; //is float
-					}
-				}
-				char* token = malloc(sizeof(char)*pos+1);
-				if(t == 'f');
-						tok = buildToken(strncpy(input, token, pos), FLOAT);
-				else
-					tok = buildToken(strncpy(input, token, pos), INT);
-				//Must check for decimal point/e in the parsing loop function
-			}
-		}//End Digit Check
-		switch(input[pos]{
+			token = malloc(sizeof(char)*len +1);
+			strncpy(token, input, len);
+			token[len] = '\0';
+			if (t == 'f')
+				tok = buildToken(token, FLOAT);
+			else
+				tok = buildToken(token, INT);
+			//Must check for decimal point/e in the parsing loop function
+		}
+	}//End Digit Check
+	else{
+		switch (input[pos]){
 			//check for the special characters, such as braces and stuffs
 		}
-		
-		return tok;
+	}
+	return tok;
 	//}while(pos < length);
- } //END function stringParse
+} //END function stringParse
 
 
 /*
- * main will have a string argument (in argv[1]).
- * The string argument contains the tokens.
- * Print out the tokens in the second string in left-to-right order.
- * Each token should be printed on a separate line.
- */
+* TKGetNextToken returns the next token from the token stream as a
+* character string.  Space for the returned token should be dynamically
+* allocated.  The caller is responsible for freeing the space once it is
+* no longer needed.
+*
+* If the function succeeds, it returns a C string (delimited by '\0')
+* containing the token.  Else it returns 0.
+*
+* You need to fill in this function as part of your implementation.
+*/
+
+char *TKGetNextToken(TokenizerT * tk) {
+	char* result = NULL;
+	if (tk != NULL){
+			//Transform tk into a string
+		result = (char*)malloc(sizeof(char)*(strlen(tk->token) + strlen(tk->type) + 3));
+		
+		strcpy(result, tk->type);
+		strcat(result, " \"");
+		strcat(result, tk->token);
+		strcat(result, "\"");
+			//destory the token tk
+		TKDestroy(tk);
+	}
+	return result;
+}
+
+/*
+* main will have a string argument (in argv[1]).
+* The string argument contains the tokens.
+* Print out the tokens in the second string in left-to-right order.
+* Each token should be printed on a separate line.
+*/
 
 int main(int argc, char **argv) {
-	if(argc != 2){
+	/*
+	if (argc != 2){
 		// input should include a string 
 		printf("Error: Invalid number of arguments.\n");
 		return 0;
 	}
-	printf("%s\n", argv[1]);
-	
+
+	printf("%s\n", argv[1]); //Taken out for inline Testing
+
 	//make the scope of argv[1] global
 	input = argv[1];
-	
-	//create a tokenizer with the given string
-	TokenizerT *tokenizer = TKCreate(input); 
-	
-	if(tokenizer != NULL){
-		//go through the string and tokenize it here
-		char *tok = TKGetNextToken(tokenizer);
-		while(tok != 0){
-			//print the token and the type
+	*/
+	input = "Hello123\n";
+	int length = strlen(input);
+	printf("%s\n", input);
+
+	//Return for strings of tokens
+	char* result = NULL;
+
+	//create a tokenizer to use
+	TokenizerT *tokenizer = NULL;
+	printf("Begin Tokenizing\n");
+	do{
+		tokenizer = stringParse(); //creates the next token
+		result = TKGetNextToken(tokenizer); //transform token into string and free
+		if (result){ //If we got a result
+			printf("%s\n", result);
 		}
-		//destroy the tokenizer. mission complete.
-		TKDestroy(tokenizer);
-	} else {
-		printf("Error building tokenizer.\n");
-	}
+	} while (pos < length);
+	printf("End Tokenizing\n");
 	
+
+	/* Sangini Code
+	if(tokenizer != NULL){
+	//go through the string and tokenize it here
+	char *tok = TKGetNextToken(tokenizer);
+	while(tok != 0){
+	//print the token and the type
+	}
+	//destroy the tokenizer. mission complete.
+	TKDestroy(tokenizer);
+	} else {
+	printf("Error building tokenizer.\n");
+	}
+	*/
+
 	return 0;
 }
 
